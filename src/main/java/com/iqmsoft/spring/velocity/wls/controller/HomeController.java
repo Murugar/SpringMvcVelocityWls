a619d77e1bdb6f151fbdd6be460733f9e11dbe9a
Davanon <davanon2014@gmail.com>
2017-03-24 16:39:48 +0530

    First

---
+++ src/main/java/com/iqmsoft/spring/velocity/wls/controller/HomeController.java
@@ -0,0 +1,35 @@
+package com.iqmsoft.spring.velocity.wls.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.iqmsoft.spring.velocity.wls.dto.Test;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController{
+
+	private Test test;
+
+	@RequestMapping(value = " ", method = RequestMethod.GET)
+	public String index(Model model) {
+		test = new Test();
+		model.addAttribute("test", test);
+
+		return "home/index";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(@Valid Test test, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "home/index";
+		}
+		return "home/view";
+	}
+
+}
---
+++ src/main/java/com/iqmsoft/spring/velocity/wls/dto/Test.java
@@ -0,0 +1,47 @@
+package com.iqmsoft.spring.velocity.wls.dto;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Test{
+	
+	@NotBlank(message = "No en blanco")
+	private String name;
+	
+	@NotBlank(message = "edad")
+	private String age;
+	
+	
+	public Test() {
+
+	}
+
+	public Test(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Test [name=" + name + "]";
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	
+	
+	
+	
+}
